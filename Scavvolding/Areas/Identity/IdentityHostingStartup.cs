f4b6b6adeba0afdad5891411a36547394be716e1
Clara Schulz-Theißen <Schulz-Theissen@eevolution.de>
2022-05-16 04:40:33 +0200

    Intigrating identity scaffold

---
+++ Scavvolding/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Scavvolding.Data;
+
+[assembly: HostingStartup(typeof(Scavvolding.Areas.Identity.IdentityHostingStartup))]
+namespace Scavvolding.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ScavvoldingContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ScavvoldingContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ScavvoldingContext>();
+            });
+        }
+    }
+}
