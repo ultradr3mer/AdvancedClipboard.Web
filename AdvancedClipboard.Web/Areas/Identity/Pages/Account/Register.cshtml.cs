8d55e55b4c44c7354d6e9d5fe185bf75d9ba0a6d
Clara Schulz-Theißen <Schulz-Theissen@eevolution.de>
2022-05-26 07:21:18 +0200

    Register fix

--- AdvancedClipboard.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ AdvancedClipboard.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -58,13 +58,13 @@ namespace AdvancedClipboard.Web.Areas.Identity.Pages.Account
             public string ConfirmPassword { get; set; }
         }
 
-        public async Task OnGetAsync(string returnUrl = null)
+        public async Task OnGetAsync(string? returnUrl = null)
         {
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
 
-        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        public async Task<IActionResult> OnPostAsync(string? returnUrl = null)
         {
             returnUrl = returnUrl ?? Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
