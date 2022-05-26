6e0dc01083a61b2798ddbd1af894ef734284a099
Clara Schulz-Theißen <Schulz-Theissen@eevolution.de>
2022-05-26 16:40:58 +0200

    Unblocked clipboard api

--- AdvancedClipboard.Web/wwwroot/js/clipboard.js
+++ AdvancedClipboard.Web/wwwroot/js/clipboard.js
@@ -2,7 +2,6 @@
 
   var flash = function (elements) {
     var mult = 2.0;
-    var color = "255, 255, 20" // has to be in this format since we use rgba
     var interval = setInterval(function () {
       mult *= 0.97;
       mult -= 0.02;
@@ -17,26 +16,25 @@
     }, 30)
   };
 
-  var form = $("#PasteForm");
-  form.submit(function () {
-    event.preventDefault();
-    form.find("#ContentToAdd").val(navigator.clipboard.readText());
-    $(this).unbind('submit').submit();
+  $(".paste-button").click(async function () {
+    var text = await navigator.clipboard.readText();
+    if (text.length == 0) {
+      window.alert("Please copy some text.");
+      return;
+    }
+    var form = $(this).parents("#PasteForm");
+    form.find("#ContentToAdd").val(text);
+    form.submit();
   });
 
-  $(".add-to-clipboard").click(function () {
+  $(".add-to-clipboard").click(async function () {
     var text = $(this).parents(".clipboard-card").find(".clipboard-text").text();
-    navigator.clipboard.writeText(text);
+    await navigator.clipboard.writeText(text);
   });
 
-  //$(".clipboard-card").click(function () {
-  //  var text = $(this).find(".clipboard-text").text();
-  //  navigator.clipboard.writeText(text);
-  //});
-
-  $(".clipboard-text").click(function () {
+  $(".clipboard-text").click(async function () {
     var text = $(this).text();
-    navigator.clipboard.writeText(text);
+    await navigator.clipboard.writeText(text);
     flash($(this).parents(".clipboard-card"));
   });
 
